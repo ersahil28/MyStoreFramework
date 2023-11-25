b7dd65db03760e882ce7935edadebf9befac215b
Sahils <ersahil28@gmail.com>
2023-11-25 16:13:23 -0500

    New Hasmap Test case

--- src/test/java/com/mystore/testcases/AccountCreationPageTest.java
+++ src/test/java/com/mystore/testcases/AccountCreationPageTest.java
@@ -6,11 +6,14 @@ import com.mystore.pageobjects.AccountCreationPage;
 import com.mystore.pageobjects.HomePage;
 import com.mystore.pageobjects.IndexPage;
 import com.mystore.pageobjects.LoginPage;
+import com.mystore.utility.Log;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 public class AccountCreationPageTest extends BaseClass {
 
@@ -38,5 +41,32 @@ public class AccountCreationPageTest extends BaseClass {
         boolean result = accountCreationPage.validateAccountCreatePage();
         Assert.assertTrue(result);
     }
+
+    @Test(groups = "Regression",dataProvider = "newAcountDetailsData",dataProviderClass = DataProviders.class)
+    public void createAccountTest(HashMap<String,String> hashMapValue) throws Throwable {
+        Log.startTestCase("createAccountTest");
+        indexPage= new IndexPage();
+        loginPage=indexPage.clickOnSignIn();
+        accountCreationPage=loginPage.createNewAccount(hashMapValue.get("Email"));
+        accountCreationPage.createAccount(
+                hashMapValue.get("Gender"),
+                hashMapValue.get("FirstName"),
+                hashMapValue.get("LastName"),
+                hashMapValue.get("SetPassword"),
+                hashMapValue.get("Day"),
+                hashMapValue.get("Month"),
+                hashMapValue.get("Year"),
+                hashMapValue.get("Company"),
+                hashMapValue.get("Address"),
+                hashMapValue.get("City"),
+                hashMapValue.get("State"),
+                hashMapValue.get("Zipcode"),
+                hashMapValue.get("Country"),
+                hashMapValue.get("MobilePhone"));
+        homePage=accountCreationPage.validateRegistration();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=my-account", homePage.getCurrentUrl());
+        Log.endTestCase("createAccountTest");
+    }
+
 
 }
